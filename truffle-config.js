11c9680418db6c6631e4e42a6c878abf7f7303f1
CherrySafe <94952923+CherrySafe@users.noreply.github.com>
2022-11-19 11:44:58 +0800

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -72,38 +72,4 @@ const mnemonic =  fs.readFileSync(".secret").toString().trim();
        }
    },
 
-   // Set default mocha options here, use special reporters etc.
-   mocha: {
-     // timeout: 100000
-   },
-
-   // Configure your compilers
-   compilers: {
-     solc: {
-       version: "0.8.10",    // Fetch exact version from solc-bin (default: truffle's version)
-       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-       // settings: {          // See the solidity docs for advice about optimization and evmVersion
-       settings: {          // See the solidity docs for advice about optimization and evmVersion
-         optimizer: {
-           enabled: true,
-           runs: 200
-         }
-       }
-       //  evmVersion: "byzantium"
-       // }
-     }
-   },
-   // api_keys: {
-   //   etherscan: etherscanApiKey
-   // },
-   // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
-   //
-   // Note: if you migrated your contracts prior to enabling this field in your Truffle project and want
-   // those previously migrated contracts available in the .db directory, you will need to run the following:
-   // $ truffle migrate --reset --compile-all
-
-   db: {
-     enabled: false
-   }
- };
 
